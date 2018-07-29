b717eabbea6521eaf2627d403c02f93685fb495a
Colin King <colinking90@gmail.com>
2018-07-28 23:37:18 -0230

    Signal class created. now to move the plotting filters and downsampled signals back to main.

---
+++ Signal.m
@@ -0,0 +1,47 @@
+classdef Signal
+    % SIGNAL creates a signal object with sampling and frequency attributes
+    %   determined by the input signal
+    
+    properties
+        samples
+        Nsamples
+        values
+        FS
+        FFT
+        Welch
+    end
+    
+    methods(Static)
+        
+        function obj = Signal(inputSignal)
+            % Construct an instance of this class
+            
+            % get the sampling properties from the input signal
+            [obj.Nsamples,~] = size(inputSignal);
+            obj.samples = inputSignal(:,1);
+            obj.values = inputSignal(:,2);
+            obj.FS = obj.Nsamples/max(obj.samples);
+            
+            % call the class function 'frequency()' to get FFT and Welch's
+            %   method for plotting frequency spectra
+            [obj.Welch,obj.FFT] = ...
+                Signal.frequency(obj.Nsamples,obj.values,obj.FS);
+        end
+        
+        function [Welch,FFT] = frequency(N_samples,Values,sample_rate)
+            % calculate the frequency properties and send to constructor
+            
+            % Parameters for Plotting FFT
+            FFT.n = 2^nextpow2(N_samples);
+            FFT.trans = fft(Values,FFT.n);
+            FFT.f = sample_rate*(0:(FFT.n/2))/FFT.n;
+            FFT.P = abs(FFT.trans/FFT.n);
+            
+            % Parameters for Plotting Pwelchs PSD
+            Nfft = 2^(floor(log(N_samples)/log(2)));
+            [Welch.PSD,Welch.f] = pwelch(Values,gausswin(Nfft),Nfft/2,Nfft,sample_rate);
+            [~,loc] = max(Welch.PSD);
+            Welch.f_est = Welch.f(loc);
+        end
+    end
+end
--- main.m
+++ main.m
@@ -1,15 +1,12 @@
 clc;clear;close all
-
+tic
 % Load Signal. The signal should be a text file with two columns
 % The file should be located in the MATLAB working directory
 % Column 1 is the sample (time, etc), Column 2 is the measurement (V, etc)
-signal = load('Em25ms10kSs.txt');
-
-% Sampling parameters of the signal
-[Nsamples,samples,voltages,FS_orig] = sampling(signal);
-
-% frequency components of the signal
-[welch,FastFT] = frequency(Nsamples,voltages,FS_orig);
+% The Signal class will create an instance of the signal object with
+%   sampling and frequency properties
+input_signal = load('Em25ms10kSs.txt');
+signal = Signal(input_signal);
 
 % display command window text to distract the impatient user
 disp('This may take a minute- chill the heck out!')
@@ -18,37 +15,38 @@ disp('This may take a minute- chill the heck out!')
 figure
 subplot(3,1,1)
 grid on
-plot (samples,voltages)
+plot(signal.samples,signal.values)
 title('Original Signal Waveform')
 xlabel('Time (seconds)'); ylabel('Voltage (mV)')
 
 % plot the Power of the frequencies from n-point fft function 
 subplot(3,1,2)
-plot(FastFT.f,FastFT.P(1:FastFT.n/2+1))
+plot(signal.FFT.f,signal.FFT.P(1:signal.FFT.n/2+1))
 title('FFT of signal')
 xlabel('Frequency (Hz)'); ylabel('|P(f)|')
 
 % plot the PSD using pwelch (should be comparable to previous fft plot)
 subplot(3,1,3)
-plot(welch.f,welch.PSD)
-title(['estimated frequency: ',num2str(welch.f_est),' hz'])
+plot(signal.Welch.f,signal.Welch.PSD)
+title(['estimated frequency: ',num2str(signal.Welch.f_est),' hz'])
 xlabel('Frequency (Hz)'); ylabel('PSD')
 pause(1)
 
 % design the filters (low-pass, high-pass, band-pass, band-stop)
-Filt = Filter.design_filters(FS_orig);
+Filt = Filter.design_filters(signal.FS);
 [~,Nfilters] = size(Filt);
 
 % plot the filter responses
 [filteredSignal,filteredSignal_adjusted] = ...
-     Filter.plot_filters(Nfilters,Filt,voltages,samples);
+     Filter.plot_filters(Nfilters,Filt,signal.values,signal.samples);
 
 % downsampled signals plotted in function, and returned to main so I can
 %   play with the signals. 
 % downsample the signal by consecutive integers and plot
-downsampled = downSample.DownSample_ones(signal,Nfilters);
+downsampled = downSample.DownSample_ones(input_signal,Nfilters);
 % downsample the signal by multiples of 2 and plot
 % introduces aliasing faster than consecutive integers
-downsampled_aliasing = downSample.DownSample_twos(signal,Nfilters);
+downsampled_aliasing = downSample.DownSample_twos(input_signal,Nfilters);
+toc
 
-
+% plot the downsampled signals
